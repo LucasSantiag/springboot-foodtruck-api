513dc88fe9a8d439257e105dc789b67b42ff7838
LucasSantiag <37224673+LucasSantiag@users.noreply.github.com>
2019-04-16 00:14:03 -0300

    Models

--- foodtruck-api/src/main/java/com/example/foodtruckapi/controller/HelloController.java
+++ foodtruck-api/src/main/java/com/example/foodtruckapi/controller/HelloController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    
+
+    @GetMapping
     public String getHello() {
         return "Hello World!";
     }
--- foodtruck-api/src/main/java/com/example/foodtruckapi/model/Ingredient.java
+++ foodtruck-api/src/main/java/com/example/foodtruckapi/model/Ingredient.java
@@ -3,19 +3,15 @@ package com.example.foodtruckapi.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
-public class Lunch {
+public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-
-    @OneToMany
-    private List<Ingredient> ingredients = new ArrayList<>();
+    private double cost;
 
 }
