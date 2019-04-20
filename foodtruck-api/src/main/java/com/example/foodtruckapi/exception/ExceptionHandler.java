39ffabfd98481ae98e90b1d41b5fdc236513e8e1
LucasSantiag <37224673+LucasSantiag@users.noreply.github.com>
2019-04-20 14:41:47 -0300

    Exception Handler

--- foodtruck-api/src/main/java/com/example/foodtruckapi/exception/ExceptionHandler.java
+++ foodtruck-api/src/main/java/com/example/foodtruckapi/exception/ExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.http.ResponseEntity;
 
-import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -13,10 +12,10 @@ import static org.springframework.http.ResponseEntity.status;
 @ControllerAdvice
 public class ExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<StandardError> objectNotFound(Exception e, HttpServletRequest request) {
-        StandardError err = new StandardError(NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());   
-        return status(NOT_FOUND).body(err);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<StandardError> objectNotFound(Exception e, HttpServletRequest request) {
+//        StandardError err = new StandardError(NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+//        return status(NOT_FOUND).body(err);
+//    }
 
 }
