4cf415b339edbefe775d73d992db9b23c6815491
LucasSantiag <37224673+LucasSantiag@users.noreply.github.com>
2019-04-22 18:32:46 -0300

    X

--- foodtruck-api/src/main/java/com/example/foodtruckapi/exception/EntityNotFoundException.java
+++ foodtruck-api/src/main/java/com/example/foodtruckapi/exception/EntityNotFoundException.java
@@ -1,8 +1,5 @@
 package com.example.foodtruckapi.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException(){
