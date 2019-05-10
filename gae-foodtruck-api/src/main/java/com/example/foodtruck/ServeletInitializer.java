242c58dce82c033c51793d1efad59f968d6019c7
Lucas Cardoso <lucassantiago17.cardoso08@gmail.com>
2019-05-10 10:59:41 -0300

    SpringBoot With GoogleAppEngine

---
+++ gae-foodtruck-api/src/main/java/com/example/foodtruck/ServeletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.foodtruck;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServeletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FoodtruckApplication.class);
+    }
+
+}
